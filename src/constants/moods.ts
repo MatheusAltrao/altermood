b7197a3095ece943f491d5b0e0b15227f529b5b4
Matheus Altrao <matheusaltrao2@gmail.com>
2025-08-27 14:53:40 -0400

    ✨ refactor(constants/moods): Atualiza descrições dos humores para maior clareza e concisão

--- src/constants/moods.ts
+++ src/constants/moods.ts
@@ -2,85 +2,40 @@ export const MOODS = [
   {
     value: "excited",
     label: "Excitado",
-    description:
-      "Responda com muito entusiasmo, energia e empolgação, transmitindo vibração positiva e intensidade.",
+    description: "Expressa entusiasmo e energia.",
     emoji: "🤩",
-  },
-  {
-    value: "happy",
-    label: "Feliz",
-    description:
-      "Responda de forma alegre e otimista, transmitindo felicidade, leveza e positividade.",
-    emoji: "😄",
-  },
-  {
-    value: "sad",
-    label: "Triste",
-    description:
-      "Responda em um tom melancólico, expressando empatia, sensibilidade e um ar de tristeza.",
-    emoji: "😢",
-  },
-  {
-    value: "angry",
-    label: "Bravo",
-    description:
-      "Responda demonstrando irritação, impaciência ou indignação, com um tom mais ríspido.",
-    emoji: "😠",
   },
   {
     value: "neutral",
     label: "Neutro",
-    description:
-      "Responda de forma objetiva, imparcial e equilibrada, sem emoção aparente.",
+    description: "Mantém um tom imparcial e objetivo.",
     emoji: "😐",
   },
   {
     value: "casual",
     label: "Casual",
     description:
-      "Responda de forma descontraída, como em uma conversa leve entre amigos, usando uma linguagem simples.",
+      "Utiliza uma linguagem descontraída e escreva como o nativo da língua seecionada, como um morador do país .",
     emoji: "😎",
   },
   {
     value: "formal",
     label: "Formal",
-    description:
-      "Responda de forma séria e profissional, com linguagem clara, polida e respeitosa.",
+    description: "Adota um tom sério e profissional.",
     emoji: "🧐",
   },
-  {
-    value: "sarcastic",
-    label: "Sarcástico",
-    description:
-      "Responda com ironia ou sarcasmo, usando humor ácido e um tom provocativo.",
-    emoji: "😏",
-  },
-  {
-    value: "humorous",
-    label: "Engraçado",
-    description:
-      "Responda de forma divertida e espirituosa, usando humor leve e criativo.",
-    emoji: "😂",
-  },
-  {
-    value: "romantic",
-    label: "Romântico",
-    description:
-      "Responda com carinho e afeto, transmitindo sentimentos amorosos e apaixonados.",
-    emoji: "😍",
-  },
+
   {
     value: "email",
     label: "Email",
-    description:
-      "Responda como se fosse uma comunicação por email: clara, objetiva, educada e organizada.",
+    description: "Adequado para comunicações por email, claro e objetivo.",
     emoji: "📧",
   },
   {
     value: "boss",
     label: "Chefe",
     description:
-      "Responda com um tom respeitoso e profissional, adequado para falar com superiores ou gestores.",
+      "Tom respeitoso e profissional para interações com superiores.",
     emoji: "👔",
   },
 ];
